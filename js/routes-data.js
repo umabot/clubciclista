37b1766f12d2a7a8bb76a0f3d587daa76ac2f22f
Umabot <umabot@users.noreply.github.com>
2026-03-22 15:44:47 +0100

    update route file

--- js/routes-data.js
+++ js/routes-data.js
@@ -6,5 +6,13 @@ window.clubRoutes = [
         distance: "15 km",
         desc: "To Cachan via paris",
         bgClass: "placeholder-bg-1"
+    },
+    {
+        title: "Paris Semlis",
+        date: "2026-05-02",
+        time: "7:00",
+        distance: "65 km",
+        desc: "To Semlis",
+        bgClass: "placeholder-bg-2"
     }
 ];

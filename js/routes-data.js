f50afc1c37e0e812bb6302076f105addaf912f43
Umabot <umabot@users.noreply.github.com>
2026-03-22 16:05:16 +0100

    update route file

--- js/routes-data.js
+++ js/routes-data.js
@@ -22,5 +22,13 @@ window.clubRoutes = [
         distance: "35 km",
         desc: "Along the ponts of Paris",
         bgClass: "placeholder-bg-3"
+    },
+    {
+        title: "Puteaux a Paris",
+        date: "2026-03-27",
+        time: "8:00",
+        distance: "15 km",
+        desc: "Ride to Paris downtown",
+        bgClass: "placeholder-bg-4"
     }
 ];

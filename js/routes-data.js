f50ceaa4dab563fe7648c83ac879195788470875
Umabot <umabot@users.noreply.github.com>
2026-03-22 14:42:45 +0100

    update route file

--- js/routes-data.js
+++ js/routes-data.js
@@ -1,10 +1,10 @@
 window.clubRoutes = [
     {
-        title: "Puteaux a Mantes-La-Jolie",
-        date: "2026-04-01",
-        time: "8:00",
-        distance: "45 km",
-        desc: "Ride ouside Paris",
-        bgClass: "placeholder-bg-3"
+        title: "Puteaux a Versailles",
+        date: "2026-04-04",
+        time: "7:00",
+        distance: "42 km",
+        desc: "Ride to Versailles",
+        bgClass: "placeholder-bg-4"
     }
 ];

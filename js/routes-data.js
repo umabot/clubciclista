fc2322edb52f80273dcec67ae5e6000ed89ce7bc
Umabot <umabot@users.noreply.github.com>
2026-03-22 14:19:55 +0100

    update route file

--- js/routes-data.js
+++ js/routes-data.js
@@ -1,10 +1,10 @@
 window.clubRoutes = [
     {
-        title: "Puteaux à Saint Cloud",
-        date: "2026-03-29",
-        time: "9:00",
-        distance: "21 km",
-        desc: "A pleasant ride next to the Seine, up to Saint Cloud and back to Puteaux",
-        bgClass: "placeholder-bg-1"
+        title: "Ascenso a Canencia",
+        date: "2026-03-27",
+        time: "8:00",
+        distance: "35 km",
+        desc: "Ascenso a Canencia via puerto",
+        bgClass: "placeholder-bg-2"
     }
 ];
